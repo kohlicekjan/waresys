97018f0380a5fcffa5c296f5765aefa9522a5e80
Jan Kohlíček <kohlicekjan@gmail.com>
2016-11-28 16:04:32 +0100

    Přidání šablon chybových stavů HTTP

---
+++ src/Server/lib/util.js
@@ -0,0 +1,6 @@
+﻿var util = require('util');
+module.exports = util;
+
+module.exports.absoluteUrl = function (req) {
+    return util.format('%s://%s%s', req.protocol, req.get('host'), req.originalUrl);
+};
